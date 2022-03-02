3741d06ec819dc5b02da4aaa18f13cc14e628125
vishnu <reddyvariabhilashreddy4722@gmail.com>
2022-03-02 17:26:25 +0530

    First Commit

---
+++ Regular/RegularDemo.java
@@ -0,0 +1,20 @@
+package Regular;
+
+import java.util.regex.*;
+
+public class RegularDemo 
+{
+
+		public static void main(String[] args) 
+		{
+
+			String pattern = "[a-z]+";
+			String check = "Regular Expressions";
+			Pattern p = Pattern.compile(pattern);
+			Matcher c = p.matcher(check);
+			
+			while (c.find())
+		      	System.out.println( check.substring( c.start(), c.end() ) );
+		}
+}
+
